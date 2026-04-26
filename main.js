e13f6ff868b64453c20039040eaa6ea40d61c41d
skvarel <skvarel@inventwo.com>
2026-04-26 10:33:21 +0200

    Added tracker map

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 "use strict";
 
-// The adapter-core module gives you access to the core ioBroker functions
-// you need to create an adapter
 const utils = require("@iobroker/adapter-core");
 
 const life360Connector = require("./lib/life360CloudConnector");
 const life360DbConnector = require("./lib/life360DbConnector");
+const Life360Tracker = require("./lib/life360Tracker"); // ← NEU
 
 class Life360 extends utils.Adapter {
 	constructor(options = {}) {
 		super(Object.assign({ name: "life360ng" }, options));
 		this.on("ready", this.onReady.bind(this));
+		this.on("stateChange", this.onStateChange.bind(this)); // ← NEU
 		this.on("unload", this.onUnload.bind(this));
 	}
 
@@ -18,17 +18,31 @@ class Life360 extends utils.Adapter {
 	 * Is called when databases are connected and adapter received configuration.
 	 */
 	async onReady() {
-		// Initialize your adapter here
-		life360Connector.setAdapter(this); //  Sets the adapter instance for the Life360 connector
+		life360Connector.setAdapter(this);
 		life360DbConnector.setAdapter(this);
 
-		// Setup polling Life360 cloud data
+		// Tracker initialisieren ← NEU
+		this.tracker = new Life360Tracker(this);
+		await this.tracker.init();
+
 		life360Connector.setupPolling(function (err, cloud_data) {
 			if (!err) {
-				//  Pass the retrieved Life360 cloud data to the DB connector.
 				life360DbConnector.publishCloudData(err, cloud_data);
 			}
 		});
+	}
+
+	/**
+	 * Is called when a subscribed state changes
+	 *
+	 * @param {string} id
+	 * @param {ioBroker.State | null | undefined} state
+	 */
+	async onStateChange(id, state) {
+		// ← NEU
+		if (this.tracker) {
+			await this.tracker.onStateChange(id, state);
+		}
 	}
 
 	/**
@@ -38,6 +52,7 @@ class Life360 extends utils.Adapter {
 	 */
 	onUnload(callback) {
 		try {
+			this.tracker?.stop(); // ← NEU
 			life360Connector.disablePolling();
 			life360Connector.disconnect();
 			this.setState("info.connection", false, true);
